e42a21d93c6cb07c2ed8d175218efe5dfbce3cf7
Andrew M. Fahmy <andrew_fahmy@outlook.com>
2017-04-10 11:09:06 +0200

    Initial Upload

---
+++ simpleddl.d.ts
@@ -0,0 +1,17 @@
+
+interface SimpleddlOptions{
+    multiple:boolean,
+    emptyResultText:string,
+    defaultOptionText:string,
+    defaultOptionValue:string,
+    search:boolean,
+    rightToLeft:boolean
+}
+
+
+
+
+
+interface JQuery {
+    Simpleddl(options: SimpleddlOptions): JQuery;
+}
